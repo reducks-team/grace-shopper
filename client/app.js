4ea7e9d33ecda826cd650257e3267cd34d2f40fa
Luke Buckheit <lukebuckheit@gmail.com>
2019-07-31 19:05:25 -0400

    fixed typo in app

--- client/app.js
+++ client/app.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Routes from './routes'
 import Navbar from './components/navbar'
 import {UserHome} from './components/user-home'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 // import FeaturedProducts from './components/FeaturedProducts'
 // import Footer from './components/Footer'
 
