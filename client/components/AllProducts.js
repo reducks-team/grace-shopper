a4d2ca1256ce784b00b5adaf81bb381e6c30b207
Johnny Kirpalani <jkirpalani@gmail.com>
2019-08-01 11:28:02 -0400

    new react updates

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -2,37 +2,22 @@
 // import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
 // import {connect} from 'react-redux'
 // //import {getAllProducts} from '../productReducer'
-// import NewStudentForm from './NewStudentForm'
 
 // export class AllStudents extends Component {
 //   componentDidMount() {
-//     this.props.getAllStudents()
+//     this.props.getAllProducts()
 //   }
 
 //   render() {
 //     return (
-//       <div className="allStudents">
-//         <h3>Student List</h3>
+//       <div className="allProducts">
+//         <h3>Products</h3>
 
-//         {this.props.AllStudents.map(student => (
-//           <div className="student.id" key={student.id}>
-//             <Link to={`/students/${student.id}`}>
-//               {student.firstName} {student.lastName}
-//             </Link>
-//             <br />
-//             <small>Remove Student</small>
-
-//             <button
-//               type="button"
-//               onClick={() => this.props.deleteStudent(student.id)}
-//             >
-//               x
-//             </button>
-//             <br />
+//         {this.props.AllProducts.map(product => (
+//           <div className="product.id" key={product.id}>
+//             <Link to={`/products/${product.id}`}>{product.name}</Link>
 //           </div>
 //         ))}
-//         <h3>Add A Student Form</h3>
-//         <NewStudentForm />
 //       </div>
 //     )
 //   }
@@ -40,15 +25,14 @@
 
 // const mapStateToProps = state => {
 //   return {
-//     AllStudents: state.students.allStudents
+//     AllProducts: state.products.allProducts
 //   }
 // }
 
 // const mapDispatchToProps = dispatch => {
 //   return {
-//     getAllStudents: () => dispatch(getAllStudents()),
-//     deleteStudent: id => dispatch(deleteStudent(id))
+//     getAllProducts: () => dispatch(getAllProducts())
 //   }
 // }
 
-// export default connect(mapStateToProps, mapDispatchToProps)(AllStudents)
+// export default connect(mapStateToProps, mapDispatchToProps)(AllProducts)
